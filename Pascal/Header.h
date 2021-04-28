dbd4a77c51d2f66a102e9d5a9c4ab66e63333e0a
SergiuPetrut <82155181+SergiuPetrut@users.noreply.github.com>
2021-04-28 20:49:57 +0300

    Add files via upload

---
+++ Pascal/Header.h
@@ -0,0 +1,1 @@
+sc#pragma once
---
+++ Pascal/pascal.h
@@ -0,0 +1,9 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define MAX 20
+#define MIN 3
+int get_int(int,int);
+int comb(int,int);
+int fact(int);
+void print_pascal(int);
